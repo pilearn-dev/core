ff8c3da59da007ef485d70398ed1a8dd4993fa31
Paul S <maus@paulstrobach.de>
2019-01-01 11:08:51 +0100

    Removed winter event 2018

--- main/__init__.py
+++ main/__init__.py
@@ -6,7 +6,7 @@ import secrets, random
 
 from controller import md, num as cnum
 from model import privileges as mprivileges, tags as mtags, user as muser, forum as mforum, proposal as mproposal, courses as mcourses, reviews as mreviews, post_templates as mpost_templates
-from view import auth as vauth, user as vuser, review as vreview, help as vhelp, courses as vcourses, forum as vforum, jsonapi as vjsonapi, survey as vsurvey, proposal as vproposal, tools as vtools, dialog as vdialog, winter2018, upload
+from view import auth as vauth, user as vuser, review as vreview, help as vhelp, courses as vcourses, forum as vforum, jsonapi as vjsonapi, survey as vsurvey, proposal as vproposal, tools as vtools, dialog as vdialog, upload
 
 from sha1 import md5
 
@@ -338,8 +338,6 @@ vproposal.apply(app)
 vtools.apply(app)
 vdialog.apply(app)
 upload.apply(app)
-
-winter2018.apply(app)
 
 @app.errorhandler(404)
 def error404(x):
