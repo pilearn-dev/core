688e930af9af4aea05c2352826c7a9b32a533c51
Paul Strobach <maus@paulstrobach.de>
2019-08-27 16:25:32 +0000

    Fix bug: Moderator tools open for wrong user when user_id>10

--- main/static/new_js/moderator.js
+++ main/static/new_js/moderator.js
@@ -1,6 +1,6 @@
 PiJS.route(/^\/user\/-?[0-9]/, function() {
   $(".js--mod-user-ops").click(function() {
-    ud = location.href.match(/\/user\/(-?[0-9])+\/(.*?)(\/|$)/)
+    ud = location.href.match(/\/user\/(-?[0-9]+)\/(.*?)(\/|$)/)
     user_id = ud[1];
     PiJS.dialog.fromURL("/tools/user/"+user_id+"/dialog", true);
     return false;
