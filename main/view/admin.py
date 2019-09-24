86e385c1404d125fed802a1740dad1705924519e
Paul S <maus@paulstrobach.de>
2019-09-24 13:01:36 +0200

    admin center

--- main/view/admin.py
+++ main/view/admin.py
@@ -8,6 +8,12 @@ from flask import request, session, redirect, url_for, abort, render_template, j
 from model import privileges as mprivileges, tags as mtags, user as muser, reviews as mreviews, forum as mforum, post_templates as mposttemplates
 from controller import num as cnum, times as ctimes
 
+def admin_index():
+    cuser = muser.getCurrentUser()
+    if not cuser.isDev():
+        abort(403)
+    return render_template("admin/index.html", title="Administration", thispage="admin")
+
 def admin_settings():
     cuser = muser.getCurrentUser()
     if not cuser.isDev():
@@ -43,5 +49,6 @@ def admin_sql():
     return render_template("admin/sql.html", title=u"Administration: SQL ausführen", lr=lr, columns=columns)
 
 def apply(app):
+    app.route('/admin', methods=["GET", "POST"])(admin_index)
     app.route('/admin/settings', methods=["GET", "POST"])(admin_settings)
     app.route('/admin/sql', methods=["GET", "POST"])(admin_sql)
