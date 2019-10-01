3d158f9f296fb5511891134fe696ac87792009b6
Paul S <maus@paulstrobach.de>
2019-10-01 19:57:27 +0200

    Translation for badges

--- main/view/badges.py
+++ main/view/badges.py
@@ -3,6 +3,8 @@ from flask import request, session, redirect, url_for, abort, render_template, j
 from model import user as muser, badges as mbadges
 from controller import num as cnum, times as ctimes
 
+from flask_babel import _
+
 import json, time
 
 def badges_page():
@@ -11,13 +13,13 @@ def badges_page():
         b = l != request.badge_prev
         request.badge_prev = l
         return b
-    return render_template("badges/list.html", title="Abzeichen", badges=sorted(mbadges.Badge.getAll(), key=lambda x:["bronze","silver","gold"].index(x.getClass())), changed=changed)
+    return render_template("badges/list.html", title=_("Abzeichen"), badges=sorted(mbadges.Badge.getAll(), key=lambda x:["bronze","silver","gold"].index(x.getClass())), changed=changed)
 
 def badges_single(id):
     if not mbadges.Badge.exists(id):
         abort(404)
     badge = mbadges.Badge(id)
-    return render_template("badges/single.html", title="Abzeichen " + badge.getName(), b=badge)
+    return render_template("badges/single.html", title=_("Abzeichen") + " - " + badge.getName(), b=badge)
 
 def badges_award(id):
     if not mbadges.Badge.exists(id):
