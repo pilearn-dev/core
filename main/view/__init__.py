39050c57c4be861c36602a960980a4157d23c6e7
Paul S <maus@paulstrobach.de>
2019-10-28 18:24:54 +0100

    Blueprint-ify + Design + Security of /contact (formerly /helpdesk)

--- main/view/__init__.py
+++ main/view/__init__.py
@@ -8,3 +8,4 @@ from proposal import proposal
 from teach import teach
 from topbar import topbar
 from help import help, legal
+from helpdesk import helpdesk
--- main/view/helpdesk.py
+++ main/view/helpdesk.py
@@ -1,5 +1,5 @@
 # coding: utf-8
-from flask import request, session, redirect, url_for, abort, render_template, jsonify
+from flask import Blueprint, request, session, redirect, url_for, abort, render_template, jsonify, escape
 from model import privileges as mprivileges, tags as mtags, user as muser, reviews as mreviews, forum as mforum, post_templates as mposttemplates
 from controller import num as cnum, times as ctimes, mail as cmail
 
@@ -9,10 +9,14 @@ from sha1 import sha1
 import json, time
 import urllib, urllib2
 
-def helpdesk_page():
+helpdesk = Blueprint('helpdesk', __name__)
+
+@helpdesk.route("/")
+def index():
     return render_template("helpdesk.html", title="Helpdesk", submit=bool(request.values.get("submit", False)))
 
-def helpdesk_submit():
+@helpdesk.route("/submit", methods=["POST"])
+def submit():
     cuser = muser.getCurrentUser()
     if not cuser.isLoggedIn():
         abort(404)
@@ -26,7 +30,7 @@ def helpdesk_submit():
 
     id = str(int(time.time()))
 
-    cmail.send_textbased_email("team@pilearn.de", u"HD #" + id + u" von Benutzer #" + str(cuser.id), u"Es gibt eine neue Support-Anfrage.\n\nTyp: " + rt + u"\n\nKommentar:\n\n" + rc + u"\n\n---\n\nAntwort an: " + rm + (" (mod)" if cuser.isMod() else "") + (" (team)" if cuser.isTeam() else ""))
+    cmail.send_textbased_email("team@pilearn.de", u"HD #" + id + u" von Benutzer #" + str(cuser.id), u"Es gibt eine neue Support-Anfrage.\n\nTyp: " + rt + u"\n\nKommentar:\n\n" + escape(rc) + u"\n\n---\n\nAntwort an: " + rm + (" (mod)" if cuser.isMod() else "") + (" (team)" if cuser.isTeam() else ""))
 
     time.sleep(1)
 
@@ -35,8 +39,4 @@ def helpdesk_submit():
     else:
         cmail.send_textbased_email(rm, "Support-Anfrage #"+id+" entgegengenommen", u"Hallo " + cuser.getDetail("realname") + u",\n\nwir haben deine Support-Anfrage vom Typ " + rt + u" entgegengenommen. Sie hat die Kennung #"+id+u". Folgendes ist dein Kommentar:\n\n"+rc+u"\n\nWenn du noch etwas hinzufügen möchtest, sende eine E-Mail mit dieser Kennung an team@pilearn.de.\n\nViele Grüße\n\n&pi;-Learn Team")
 
-    return redirect(url_for("helpdesk_page", submit=True))
-
-def apply(app):
-    app.route('/helpdesk')(helpdesk_page)
-    app.route('/helpdesk/submit', methods=["POST"])(helpdesk_submit)
+    return redirect(url_for("helpdesk.index", submit=True))
